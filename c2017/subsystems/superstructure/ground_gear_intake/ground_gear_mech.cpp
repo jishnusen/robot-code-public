9b9973b6b6e59b245b16ba8d3547f959a8337bdc
Apple61752 <mohamedapple@hotmail.com>
2017-01-14 16:49:45 -0800

    Add files and really basic starting code within

---
+++ c2017/subsystems/superstructure/ground_gear_intake/ground_gear_mech.cpp
@@ -0,0 +1,19 @@
+#include "ground_gear_mech.h"
+#include <iostream>
+
+namespace c2017 {
+
+namespace ground_gear_mech {
+
+class GroundGearIntake {
+ public:
+  void SetGoal() {
+    //stuff
+  }
+  double Update() {
+    //stuff
+  }
+}
+
+
+
---
+++ c2017/subsystems/superstructure/ground_gear_intake/ground_gear_mech.h
@@ -0,0 +1,17 @@
+#ifndef C2017_SUBSYSTEMS_SUPERSTRUCTURE_GROUNDGEARINTAKE_H_
+#define C2017_SUBSYSTEMS_SUPERSTRUCTURE_GROUNDGEARINTAKE_H_
+
+#include "queue_types.h"
+
+namespace c2017 {
+
+namespace ground_gear_mech {
+
+class GroundGearIntake {
+ public:
+  void SetGoal();
+
+  double Update();
+
+
+#endif //C2017_SUBSYSTEMS_SUPERSTRUCTURE_GROUNDGEARINTAKE_H_
