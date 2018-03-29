cf4d070578dcc2e5e7f56a8c90e4fe81db124d64
jishnusen <jishnu1@gmail.com>
2018-03-28 20:34:52 -0700

    Linting

--- c2018/subsystems/score_subsystem/elevator/elevator.h
+++ c2018/subsystems/score_subsystem/elevator/elevator.h
@@ -54,7 +54,8 @@ class ElevatorController {
   void SetGoal(double goal);  // Setter for unprofiled_goal_ that also caps it
                               // to kElevatorMin and Max Height
 
-  muan::units::Time TimeLeftUntil(muan::units::Length target, muan::units::Length final_goal);
+  muan::units::Time TimeLeftUntil(muan::units::Length target,
+                                  muan::units::Length final_goal);
 
   bool is_calibrated() const;  // Getter for if it's calibrated
 
