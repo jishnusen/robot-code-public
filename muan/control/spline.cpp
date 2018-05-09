6ee73d9e87e367b6ea1b41c2119ee32f82e85455
jishnusen <jishnu1@gmail.com>
2018-05-09 16:26:09 -0700

    temp

--- muan/control/spline.cpp
+++ muan/control/spline.cpp
@@ -14,13 +14,32 @@ QuinticHermiteSpline::QuinticHermiteSpline(Pose p0, Pose p1) {
   double scale =
       1.2 * ::std::abs((p0.translational() - p1.translational()).norm());
 
-  p0_.block<2, 1>(0, 0) = p0.translational();
-  p0_.block<2, 1>(3, 0) = FromMagDirection(scale, p0.heading());
-  p0_.block<2, 1>(5, 0) = Eigen::Matrix<double, 2, 1>::Zero();
+  position_0_ = p0.translational();
+  velocity_0_ = FromMagDirection(scale, p0.heading());
+  accel_0_ = Eigen::Matrix<double, 2, 1>::Zero();
 
-  p1_.block<2, 1>(0, 0) = p1.translational();
-  p1_.block<2, 1>(3, 0) = FromMagDirection(scale, p1.heading());
-  p1_.block<2, 1>(5, 0) = Eigen::Matrix<double, 2, 1>::Zero();
+  position_1_ = p1.translational();
+  velocity_1_ = FromMagDirection(scale, p1.heading());
+  accel_1_ = Eigen::Matrix<double, 2, 1>::Zero();
+
+  ComputeCoefficients();
+}
+
+void QuinticHermiteSpline::ComputeCoefficients() {
+  a_ = -6 * position_0_ - 3 * velocity_0_ - 0.5 * accel_0_ + 0.5 * accel_1_ -
+       3 * velocity_1_ + 6 * position_1_;
+
+  b_ = 15 * position_0_ + 8 * velocity_0_ + 1.5 * accel_0_ - accel_1_ +
+       7 * velocity_1_ - 15 * position_1_;
+
+  c_ = -10 * position_0_ - 6 * velocity_0_ - 1.5 * accel_0_ + 0.5 * accel_1_ -
+       4 * velocity_1_ + 10 * position_1_;
+
+  d_ = 0.5 * accel_0_;
+
+  e_ = velocity_0_;
+
+  f_ = position_0_;
 }
 
 }  // namespace control
--- muan/control/spline.h
+++ muan/control/spline.h
@@ -19,7 +19,64 @@ class QuinticHermiteSpline {
   QuinticHermiteSpline(Pose p0, Pose p1);
 
   Pose get_start_pose() const {
-    return Pose(p0_.block<2, 1>(0, 0), ::std::atan2(p0_(1, 0), p0_(0, 0)));
+    return Pose(position_0_, ::std::atan2(position_0_(1), position_0_(0)));
+  }
+
+  Pose get_end_pose() const {
+    return Pose(position_1_, ::std::atan2(position_1_(1), position_1_(0)));
+  }
+
+  Eigen::Vector2d PointAt(double t) {
+    return a_ * t * t * t * t * t + b_ * t * t * t * t + c_ * t * t * t +
+           d_ * t * t + e_ * t + f_;
+  }
+
+  Eigen::Vector2d VelocityAt(double t) {
+    return 5 * a_ * t * t * t * t + 4 * b_ * t * t * t + 3 * c_ * t * t +
+           2 * d_ * t + e_;
+  }
+
+  Eigen::Vector2d AccelAt(double t) {
+    return 20 * a_ * t * t * t + 12 * b_ * t * t + 6 * c_ * t + 2 * d_;
+  }
+
+  Eigen::Vector2d JerkAt(double t) {
+    return 60 * a_ * t * t + 24 * b_ * t + 6 * c_;
+  }
+
+  double CurvatureAt(double t) {
+    double dx = VelocityAt(t)(0);
+    double dy = VelocityAt(t)(1);
+    double ddx = AccelAt(t)(0);
+    double ddy = AccelAt(t)(1);
+
+    return (dx * ddy - ddx * dy) /
+           ((dx * dx + dy * dy) * ::std::sqrt((dx * dx + dy * dy)));
+  }
+  double DCurvatureAt(double t) {
+    double dx = VelocityAt(t)(0);
+    double dy = VelocityAt(t)(1);
+    double ddx = AccelAt(t)(0);
+    double ddy = AccelAt(t)(1);
+    double dddx = JerkAt(t)(0);
+    double dddy = JerkAt(t)(1);
+    double dx2dy2 = (dx * dx + dy * dy);
+    double num = (dx * dddy - dddx * dy) * dx2dy2 -
+                 3 * (dx * ddy - ddx * dy) * (dx * ddx + dy * ddy);
+    return num / (dx2dy2 * dx2dy2 * ::std::sqrt(dx2dy2));
+  }
+
+  double DCurvature2At(double t) {
+    double dx = VelocityAt(t)(0);
+    double dy = VelocityAt(t)(1);
+    double ddx = AccelAt(t)(0);
+    double ddy = AccelAt(t)(1);
+    double dddx = JerkAt(t)(0);
+    double dddy = JerkAt(t)(1);
+    double dx2dy2 = (dx * dx + dy * dy);
+    double num = (dx * dddy - dddx * dy) * dx2dy2 -
+                 3 * (dx * ddy - ddx * dy) * (dx * ddx + dy * ddy);
+    return num * num / (dx2dy2 * dx2dy2 * dx2dy2 * dx2dy2 * dx2dy2);
   }
 
  private:
@@ -29,7 +86,9 @@ class QuinticHermiteSpline {
   void ComputeCoefficients();
 
   Eigen::Vector2d a_, b_, c_, d_, e_, f_;
-  Eigen::Vector6d p0_, p1_;  // (x, y, x', y', x'', y'')
+  Eigen::Vector2d position_0_, position_1_;  // (x, y)
+  Eigen::Vector2d velocity_0_, velocity_1_;  // (x', y')
+  Eigen::Vector2d accel_0_, accel_1_;        // (x'', y'')
 };
 
 }  // namespace control
