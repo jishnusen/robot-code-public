35f5892939fb409fdb7bcfeb0bfddc3d52ddf334
jishnusen <jishnu1@gmail.com>
2018-05-17 19:38:00 -0700

    Add timed pose and Matrix Get() method to PoseWithCurvature

--- muan/control/pose.h
+++ muan/control/pose.h
@@ -38,7 +38,7 @@ class PoseWithCurvature {
  public:
   PoseWithCurvature() = default;
   PoseWithCurvature(Pose pose, double curvature, double dcurvature_ds);
-  
+
   PoseWithCurvature operator+(const PoseWithCurvature &other) const;
   PoseWithCurvature operator-(const PoseWithCurvature &other) const;
 
@@ -46,12 +46,17 @@ class PoseWithCurvature {
 
   PoseWithCurvature Interpolate(PoseWithCurvature other, double frac);
 
-  inline PoseWithCurvature Get() const {
-    return PoseWithCurvature(pose_, curvature_, dcurvature_ds_);
+  inline Eigen::Vector2d translational() const { return pose_.translational(); }
+  inline double heading() const { return pose_.heading(); }
+
+  inline Eigen::Matrix<double, 5, 1> Get() const {
+    return (Eigen::Matrix<double, 5, 1>() << pose_.Get(), curvature_,
+            dcurvature_ds_)
+        .finished();
   }
   inline Pose pose() const { return pose_; }
   inline double curvature() const { return curvature_; }
-  inline double dcurvature_ds() const {return dcurvature_ds_; }
+  inline double dcurvature_ds() const { return dcurvature_ds_; }
 
  private:
   Pose pose_;
@@ -59,7 +64,33 @@ class PoseWithCurvature {
   double dcurvature_ds_;
 };
 
+template <typename T>
+class TimedPose {
+ public:
+  TimedPose(T pose);
+  TimedPose(T pose, double t, double velocity, double acceleration);
+
+  inline T pose();
+  inline double t() { return t_; }
+  inline double velocity() { return velocity_; }
+  inline double acceleration() { return acceleration_; }
+
+  void set_t(double t) { t_ = t; }
+  void set_velocity(double velocity) { velocity_ = velocity; }
+  void set_acceleration(double acceleration) { acceleration_ = acceleration; }
+
+  TimedPose<T> Interpolate(TimedPose<T> other, double frac);
+
+ private:
+  T pose_;
+  double t_;
+  double velocity_;
+  double acceleration_;
+};
+
 }  // namespace control
 }  // namespace muan
+
+#include "muan/control/pose.hpp"
 
 #endif  // MUAN_CONTROL_POSE_H_
