dacb730d019a67c395f5a02acf75a52816cceb5a
jishnusen <jishnu1@gmail.com>
2018-05-27 17:24:41 -0700

    Update wrist controller

--- c2018_rewrite/subsystems/score_subsystem/wrist/wrist.cpp
+++ c2018_rewrite/subsystems/score_subsystem/wrist/wrist.cpp
@@ -2,21 +2,69 @@
 
 namespace c2018 {
 namespace subsystems {
+namespace wrist {
+
+Wrist::Wrist() {
+  std::lock_guard<std::mutex> lock(talon_lock_);
+  claw_->ConfigSelectedFeedbackSensor(FeedbackDevice::CTRE_MagEncoder_Relative,
+                                       0, 100);
+  claw_->Config_kP(kNoCubeSlot, kPNoCube, 100);
+  claw_->Config_kI(kNoCubeSlot, kINoCube, 100);
+  claw_->Config_kD(kNoCubeSlot, kDNoCube, 100);
+  claw_->Config_kF(kNoCubeSlot, kFNoCube, 100);
+
+  claw_->ConfigMaxIntegralAccumulator(kNoCubeSlot, kMaxIntegral, 100);
+  claw_->Config_IntegralZone(kNoCubeSlot, kIZone, 100);
+  claw_->ConfigAllowableClosedloopError(kNoCubeSlot, kDeadband, 100);
+
+  claw_->Config_kP(kCubeSlot, kPCube, 100);
+  claw_->Config_kI(kCubeSlot, kICube, 100);
+  claw_->Config_kD(kCubeSlot, kDCube, 100);
+  claw_->Config_kF(kCubeSlot, kFCube, 100);
+
+  claw_->ConfigMaxIntegralAccumulator(kCubeSlot, kMaxIntegral, 100);
+  claw_->Config_IntegralZone(kCubeSlot, kIZone, 100);
+  claw_->ConfigAllowableClosedloopError(kCubeSlot, kDeadband, 100);
+}
 
 Wrist& Wrist::GetInstance() {
   static Wrist instance;
   return instance;
 }
 
+void Wrist::ReadInputs() {
+  input_.encoder = claw_->GetSelectedSensorPosition(0);
+  input_.cube_proxy = carriage_canifier_.input().cube_proxy;
+  input_.hall_effect = carriage_canifier_.input().wrist_hall_effect;
+}
+
+void Wrist::SetGoal(double wrist_angle, IntakeGoal intake_mode) {
+  // Cap unprofiled goal to keep things safe
+  unprofiled_goal_ = {muan::utils::Cap(wrist_angle, kMinAngle, kMaxAngle), 0.};
+  // Set the goal intake mode
+  intake_mode_ = intake_mode;
+}
+
 void Wrist::Update(bool outputs_enabled) {
-  bool wrist_solenoid_close = false;
-  bool wrist_solenoid_open = false;
+  std::lock_guard<std::mutex> lock(talon_lock_);
+  ReadInputs();
+
+  bool intake_solenoid_close = false;
+  bool intake_solenoid_open = false;
+
+  bool was_calibrated = is_calibrated();
+  double calibrated_encoder =
+      hall_calibration_.Update(input_.encoder, input_.hall_effect);
 
   if (!outputs_enabled) {
-    wrist_->SetSelectedSensorPosition(
-        hall_calibration_.Update(input_.encoder, input_.hall_effect), 0, 0);
-    profiled_goal_ = {wrist_->GetSelectedSensorPosition(0) * kWristAngleFactor,
-                      wrist_->GetSelectedSensorVelocity(0) * kWristVelFactor};
+    profiled_goal_ = {claw_->GetSelectedSensorPosition(0) * kAngleFactor,
+                      claw_->GetSelectedSensorVelocity(0) * kVelFactor};
+  }
+
+  if (!was_calibrated && is_calibrated()) {
+    claw_->SetSelectedSensorPosition(calibrated_encoder, 0, 0);
+    profiled_goal_ = {claw_->GetSelectedSensorPosition(0) * kAngleFactor,
+                      claw_->GetSelectedSensorVelocity(0) * kVelFactor};
   }
 
   bool has_cube = pinch_state_ == IDLE_WITH_CUBE && input_.cube_proxy;
@@ -25,34 +73,34 @@ void Wrist::Update(bool outputs_enabled) {
     switch (intake_mode_) {
       case IntakeGoal::INTAKE:
         intake_voltage_ = kIntakeVoltage;
-        wrist_solenoid_close = false;
-        wrist_solenoid_open = false;
+        intake_solenoid_close = false;
+        intake_solenoid_open = false;
         break;
       case IntakeGoal::INTAKE_OPEN:
         intake_voltage_ = kIntakeVoltage;
-        wrist_solenoid_close = false;
-        wrist_solenoid_open = true;
+        intake_solenoid_close = false;
+        intake_solenoid_open = true;
         break;
       case IntakeGoal::INTAKE_CLOSE:
       case IntakeGoal::SETTLE:
         intake_voltage_ = kIntakeVoltage;
-        wrist_solenoid_close = true;
-        wrist_solenoid_open = false;
+        intake_solenoid_close = true;
+        intake_solenoid_open = false;
         break;
       case IntakeGoal::OUTTAKE_SLOW:
         intake_voltage_ = kSlowOuttakeVoltage;
-        wrist_solenoid_close = true;
-        wrist_solenoid_open = false;
+        intake_solenoid_close = true;
+        intake_solenoid_open = false;
         break;
       case IntakeGoal::OUTTAKE_FAST:
         intake_voltage_ = kFastOuttakeVoltage;
-        wrist_solenoid_close = true;
-        wrist_solenoid_open = false;
+        intake_solenoid_close = true;
+        intake_solenoid_open = false;
         break;
       case IntakeGoal::DROP:
         intake_voltage_ = 0;
-        wrist_solenoid_close = false;
-        wrist_solenoid_open = true;
+        intake_solenoid_close = false;
+        intake_solenoid_open = true;
         break;
       case IntakeGoal::INTAKE_NONE:
         if (has_cube) {
@@ -60,8 +108,8 @@ void Wrist::Update(bool outputs_enabled) {
         } else {
           intake_voltage_ = 0;
         }
-        wrist_solenoid_close = true;
-        wrist_solenoid_open = false;
+        intake_solenoid_close = true;
+        intake_solenoid_open = false;
         break;
     }
   } else {
@@ -70,7 +118,7 @@ void Wrist::Update(bool outputs_enabled) {
 
   switch (pinch_state_) {
     case MOVING:
-      if (wrist_solenoid_close) {
+      if (intake_solenoid_close) {
         has_cube_for_ticks_--;
       } else {
         has_cube_for_ticks_ = kNumHasCubeTicks;
@@ -80,27 +128,52 @@ void Wrist::Update(bool outputs_enabled) {
       }
       break;
     case IDLE_WITH_CUBE:
-      if (wrist_solenoid_open) {
+      if (intake_solenoid_open) {
         pinch_state_ = IDLE_NO_CUBE;
       }
       break;
     case IDLE_NO_CUBE:
-      if (wrist_solenoid_close) {
+      if (intake_solenoid_close) {
         pinch_state_ = MOVING;
       }
       break;
   }
 
-  UpdateProfiledGoal(outputs_enabled);
+  SetGains(has_cube);
 
-  wrist_->Set(ControlMode::Position, profiled_goal_.position,
-              DemandType_ArbitraryFeedForward, CalculateFeedForwards());
+  if (is_calibrated()) {
+    UpdateProfiledGoal(outputs_enabled);
+
+    claw_->Set(ControlMode::Position, profiled_goal_.position / kAngleFactor,
+                DemandType_ArbitraryFeedForward, CalculateFeedForwards());
+  } else {
+    claw_->Set(ControlMode::PercentOutput, kCalibVoltage / 12.);
+  }
+
+  intake_->Set(ControlMode::PercentOutput, intake_voltage_ / 12.);
+
+  status_.angle = input_.encoder * kAngleFactor;
+  status_.has_cube = has_cube;
+  status_.is_calibrated = is_calibrated();
+  status_.unprofiled_goal = unprofiled_goal_.position;
+  status_.profiled_goal = profiled_goal_.position;
+
+  output_.claw_voltage = claw_->GetMotorOutputVoltage();
+  output_.claw_percent = claw_->GetMotorOutputPercent();
+  output_.claw_current = claw_->GetOutputCurrent();
+
+  output_.intake_voltage = intake_->GetMotorOutputVoltage();
+  output_.intake_percent = intake_->GetMotorOutputPercent();
+  output_.intake_current = intake_->GetOutputCurrent();
+
+  output_.intake_solenoid_open = intake_solenoid_open;
+  output_.intake_solenoid_close = intake_solenoid_close;
 }
 
 void Wrist::UpdateProfiledGoal(bool outputs_enabled) {
   muan::control::TrapezoidalMotionProfile profile =
-      muan::control::TrapezoidalMotionProfile(kWristConstraints,
-                                              unprofiled_goal_, profiled_goal_);
+      muan::control::TrapezoidalMotionProfile(kConstraints, unprofiled_goal_,
+                                              profiled_goal_);
   if (outputs_enabled) {
     profiled_goal_ = profile.Calculate(10 * muan::units::ms);
   } else {
@@ -110,5 +183,21 @@ void Wrist::UpdateProfiledGoal(bool outputs_enabled) {
 
 double Wrist::CalculateFeedForwards() { return 0.; }
 
+double Wrist::TimeLeftUntil(double angle, double final_angle) {
+  if (profiled_goal_.position > angle) {
+    return 0.; // We don't care about the backwards profile; we're safe
+  }
+
+  muan::control::TrapezoidalMotionProfile profile =
+      muan::control::TrapezoidalMotionProfile(kConstraints, {final_angle, 0},
+                                              profiled_goal_);
+  return profile.TimeLeftUntil(angle);
+}
+
+void Wrist::SetGains(bool has_cube) {
+  claw_->SelectProfileSlot(has_cube ? kCubeSlot : kNoCubeSlot, 0);
+}
+
+}  // namespace wrist
 }  // namespace subsystems
 }  // namespace c2018
--- c2018_rewrite/subsystems/score_subsystem/wrist/wrist.h
+++ c2018_rewrite/subsystems/score_subsystem/wrist/wrist.h
@@ -1,26 +1,29 @@
-#ifndef C2018_SUBSYSTEMS_SCORE_SUBSYSTEM_WRIST_WRIST_H_
-#define C2018_SUBSYSTEMS_SCORE_SUBSYSTEM_WRIST_WRIST_H_
+#ifndef C2018_REWRITE_SUBSYSTEMS_SCORE_SUBSYSTEM_WRIST_WRIST_H_
+#define C2018_REWRITE_SUBSYSTEMS_SCORE_SUBSYSTEM_WRIST_WRIST_H_
 
-#include "c2018_rewrite/subsystems/constants.h"
+#include <mutex>
+#include "c2018_rewrite/subsystems/score_subsystem/carriage_canifier/carriage_canifier.h"
 #include "ctre/Phoenix.h"
 #include "muan/control/calibration/hall_calibration.h"
 #include "muan/control/trapezoidal_motion_profile.h"
+#include "muan/phoenix/sp_factory.h"
 #include "muan/units/units.h"
 #include "muan/utils/math_utils.h"
 
 namespace c2018 {
 namespace subsystems {
+namespace wrist {
 
 // Parameters for the trapezoidal profile
-constexpr double kMaxWristVelocity = 8.0;
-constexpr double kMaxWristAcceleration = 8.0;
-constexpr muan::control::MotionProfileConstraints kWristConstraints = {
-    kMaxWristVelocity, kMaxWristAcceleration};
+constexpr double kMaxVelocity = 8.0;
+constexpr double kMaxAcceleration = 8.0;
+constexpr muan::control::MotionProfileConstraints kConstraints = {
+    kMaxVelocity, kMaxAcceleration};
 
 // Angle constants
-constexpr double kWristMinAngle = 0;
-constexpr double kWristStowAngle = 80 * (M_PI / 180);
-constexpr double kWristMaxAngle = 160 * (M_PI / 180);
+constexpr double kMinAngle = 0;
+constexpr double kStowAngle = 80 * (M_PI / 180);
+constexpr double kMaxAngle = 160 * (M_PI / 180);
 
 // Hall Calibration constants
 constexpr double kCalibVoltage = 0;
@@ -37,6 +40,33 @@ constexpr double kMaxVoltage = 12;
 
 // Has Cube encoder stuff
 constexpr int kNumHasCubeTicks = 150;
+
+// CAN ID
+constexpr int kWristId = 7;
+constexpr int kIntakeId = 8;
+
+// PID Gains
+constexpr double kMaxIntegral = 500000.;
+constexpr double kIZone = 500.;
+constexpr double kDeadband = 5;
+
+constexpr double kPNoCube = 3.;
+constexpr double kINoCube = 0.;
+constexpr double kDNoCube = 50.;
+constexpr double kFNoCube = 1.05;
+
+constexpr double kPCube = 3.;
+constexpr double kICube = 0.;
+constexpr double kDCube = 50.;
+constexpr double kFCube = 1.05;
+
+// PID misc
+constexpr double kSensorRatio = 17.04;
+constexpr double kVelFactor = (2 * M_PI) / 4096. / 0.1 / kSensorRatio;
+constexpr double kAngleFactor = (2 * M_PI) / 4096. / kSensorRatio;
+
+constexpr int kNoCubeSlot = 0;
+constexpr int kCubeSlot = 1;
 
 enum IntakeGoal {
   INTAKE_NONE,
@@ -70,24 +100,44 @@ struct WristStatus {
   double profiled_goal;
 };
 
+struct WristOutput {
+  double claw_voltage;
+  double claw_percent;
+  double claw_current;
+
+  double intake_voltage;
+  double intake_percent;
+  double intake_current;
+
+  bool intake_solenoid_open;
+  bool intake_solenoid_close;
+};
+
 class Wrist {
  public:
   static Wrist& GetInstance();
 
   void SetGoal(double angle, IntakeGoal mode);
   void Update(bool outputs_enabled);
-
   double TimeLeftUntil(double angle, double final_angle);
 
-  bool is_calibrated() const;
+  inline bool is_calibrated() const {
+    return hall_calibration_.is_calibrated();
+  }
+  inline WristInput input() const { return input_; }
+  inline WristStatus status() const { return status_; }
+  inline WristOutput output() const { return output_; }
 
  private:
   Wrist();
   double CalculateFeedForwards();
   void SetGains(bool has_cube);
   void UpdateProfiledGoal(bool outputs_enabled);
+  void ReadInputs();
 
-  TalonSRX* wrist_ = new TalonSRX(kWristId);
+  TalonSRX* claw_ =
+      muan::phoenix::CreateTalon(kWristId, muan::phoenix::SPConfig());
+  TalonSRX* intake_ = new TalonSRX(kIntakeId);
 
   muan::control::MotionProfilePosition unprofiled_goal_;
   muan::control::MotionProfilePosition profiled_goal_;
@@ -97,14 +147,21 @@ class Wrist {
   IntakeGoal intake_mode_ = IntakeGoal::INTAKE_NONE;
   PinchState pinch_state_ = PinchState::IDLE_WITH_CUBE;
 
+  carriage_canifier::CarriageCanifier& carriage_canifier_ =
+      carriage_canifier::CarriageCanifier::GetInstance();
+
   double intake_voltage_ = 0;
   int has_cube_for_ticks_ = 0;
 
   WristInput input_;
   WristStatus status_;
+  WristOutput output_;
+
+  std::mutex talon_lock_;
 };
 
+}  // namespace wrist
 }  // namespace subsystems
 }  // namespace c2018
 
-#endif  // C2018_SUBSYSTEMS_SCORE_SUBSYSTEM_WRIST_WRIST_H_
+#endif  // C2018_REWRITE_SUBSYSTEMS_SCORE_SUBSYSTEM_WRIST_WRIST_H_
