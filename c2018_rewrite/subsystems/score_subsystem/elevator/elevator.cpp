3eb4be8b40cc38a2f78b0162c1cf425bfdfad43c
jishnusen <jishnu1@gmail.com>
2018-05-30 08:45:37 -0700

    Add elevator subsystem

--- c2018_rewrite/subsystems/score_subsystem/elevator/elevator.cpp
+++ c2018_rewrite/subsystems/score_subsystem/elevator/elevator.cpp
@@ -1,9 +1,109 @@
-#include "c2018/subsystems/score_subsystem/elevator/elevator.h"
+#include "c2018_rewrite/subsystems/score_subsystem/elevator/elevator.h"
 
 namespace c2018 {
-namespace score_subsystem {
+namespace subsystems {
 namespace elevator {
 
+Elevator::Elevator() {
+  std::lock_guard<std::mutex> lock(talon_lock_);
+  muan::phoenix::SPConfig config;
+  config.closed_loop_ramp_rate = kRampTime;
+  config.open_loop_ramp_rate = kRampTime;
+
+  elevator_ = muan::phoenix::TalonWrapper(kId, config);
+
+  elevator_.SetFeedbackSensor(FeedbackDevice::CTRE_MagEncoder_Relative);
+  elevator_.SetGains(kGains, 0);
+}
+
+Elevator& Elevator::GetInstance() {
+  static Elevator instance;
+  return instance;
+}
+
+void Elevator::ReadInputs() {
+  input_.encoder = elevator_.position();
+  input_.hall_effect = carriage_canifier_.input().elevator_hall_effect;
+}
+
+void Elevator::SetGoal(double height) {
+  unprofiled_goal_ = {muan::utils::Cap(height, kMinHeight, kMaxHeight), 0.};
+}
+
+void Elevator::Update(bool outputs_enabled) {
+  std::lock_guard<std::mutex> lock(talon_lock_);
+  ReadInputs();
+
+  bool was_calibrated = is_calibrated();
+  double calibrated_encoder =
+      hall_calibration_.Update(input_.encoder, input_.hall_effect);
+
+  if (!outputs_enabled) {
+    profiled_goal_ = {elevator_.position() * kHeightFactor,
+                      elevator_.velocity() * kVelFactor};
+  }
+
+  if (!was_calibrated && is_calibrated()) {
+    elevator_.ResetSensor(calibrated_encoder);
+    profiled_goal_ = {elevator_.position() * kHeightFactor,
+                      elevator_.velocity() * kVelFactor};
+  }
+
+  if (is_calibrated()) {
+    UpdateProfiledGoal(outputs_enabled);
+
+    elevator_.SetPosition(
+        profiled_goal_.position / kHeightFactor,
+        CalculateFeedForwards(carriage_canifier_.input().cube_proxy,
+                              profiled_goal_.position > kSecondStageHeight));
+  } else {
+    elevator_.SetOpenloop(kCalibVoltage / 12.);
+  }
+
+  status_.height = input_.encoder * kHeightFactor;
+  status_.is_calibrated = is_calibrated();
+  status_.unprofiled_goal = unprofiled_goal_.position;
+  status_.profiled_goal = profiled_goal_.position;
+
+  output_.voltage = elevator_.voltage();
+  output_.percent = elevator_.percent();
+  output_.current = elevator_.current();
+}
+
+void Elevator::UpdateProfiledGoal(bool outputs_enabled) {
+  muan::control::TrapezoidalMotionProfile profile =
+      muan::control::TrapezoidalMotionProfile(kConstraints, unprofiled_goal_,
+                                              profiled_goal_);
+  if (outputs_enabled) {
+    profiled_goal_ = profile.Calculate(10 * muan::units::ms);
+  } else {
+    profiled_goal_ = profile.Calculate(0);
+  }
+}
+
+muan::units::Time Elevator::TimeLeftUntil(double target,
+                                                    double final_goal) {
+  if (profiled_goal_.position > target) {
+    return 0.;
+  }
+  muan::control::TrapezoidalMotionProfile profile =
+      muan::control::TrapezoidalMotionProfile(kConstraints, {final_goal, 0},
+                                              profiled_goal_);
+  return profile.TimeLeftUntil(target);
+}
+
+double CalculateFeedForwards(bool has_cube, bool second_stage) {
+  double ff = kFF;
+  if (has_cube) {
+    ff += kFFCube;
+  }
+  if (second_stage) {
+    ff += kFFSecondStage;
+  }
+
+  return ff;
+}
+
 }  // namespace elevator
-}  // namespace score_subsystem
+}  // namespace subsystems
 }  // namespace c2018
--- c2018_rewrite/subsystems/score_subsystem/elevator/elevator.h
+++ c2018_rewrite/subsystems/score_subsystem/elevator/elevator.h
@@ -1,76 +1,133 @@
-#ifndef C2018_SUBSYSTEMS_SCORE_SUBSYSTEM_ELEVATOR_ELEVATOR_H_
-#define C2018_SUBSYSTEMS_SCORE_SUBSYSTEM_ELEVATOR_ELEVATOR_H_
+#ifndef C2018_REWRITE_SUBSYSTEMS_SCORE_SUBSYSTEM_ELEVATOR_ELEVATOR_H_
+#define C2018_REWRITE_SUBSYSTEMS_SCORE_SUBSYSTEM_ELEVATOR_ELEVATOR_H_
 
-#include <cmath>
-#include <limits>
-
+#include <mutex>
+#include "c2018_rewrite/subsystems/score_subsystem/carriage_canifier/carriage_canifier.h"
+#include "ctre/Phoenix.h"
 #include "muan/control/calibration/hall_calibration.h"
 #include "muan/control/trapezoidal_motion_profile.h"
+#include "muan/phoenix/talon_wrapper.h"
 #include "muan/units/units.h"
 #include "muan/utils/math_utils.h"
 
 namespace c2018 {
-namespace score_subsystem {
+namespace subsystems {
 namespace elevator {
 
+constexpr double kPitchRadius = (1. + (1. / 16.)) * 0.0254;
+constexpr double kSensorRatio = 2.14;
+constexpr double kHeightFactor = kPitchRadius * (2 * M_PI) / 512 / kSensorRatio;
+constexpr double kVelFactor = kHeightFactor / 0.1;  // native / 100ms
+
 // Trapezoid Profile parameter
-constexpr double kElevatorMaxAcceleration = 4.0 * muan::units::mps2;
-constexpr double kElevatorMaxVelocity = 2.5 * muan::units::mps;
-constexpr muan::control::MotionProfileConstraints kElevatorConstraints = {
-    .max_velocity = kElevatorMaxVelocity,
-    .max_acceleration = kElevatorMaxAcceleration};
+constexpr double kMaxAcceleration = 4.0 * muan::units::mps2;
+constexpr double kMaxVelocity = 2.5 * muan::units::mps;
+constexpr muan::control::MotionProfileConstraints kConstraints = {
+    .max_velocity = kMaxVelocity, .max_acceleration = kMaxAcceleration};
 
 // Capping stuff so it doesn't go boom
-constexpr double kElevatorMinHeight = 0.0;
-constexpr double kElevatorMaxHeight = 1.92;
+constexpr double kMinHeight = 0.0;
+constexpr double kMaxHeight = 1.92;
+constexpr double kSecondStageHeight = 1.;
 
 // Realistic voltage (SKY)
-constexpr double kElevatorMaxVoltage = 12;
+constexpr double kMaxVoltage = 12;
 
 // Calibration parameters so it thinks it is where it actually is
-constexpr double kHallEffectHeight = 0.92;
-constexpr double kCalibrationVoltage = 6;
+constexpr double kHallEffectHeight = 0.92 / kHeightFactor;
+constexpr double kCalibVoltage = 6;
 
 // Encoder fault stuff so it doesn't get too sad when they break
 constexpr double kEncoderFaultMinVoltage = 6;
 constexpr double kEncoderFaultTicksAllowed = 100;
 
-class ElevatorController {
+// CAN ID
+constexpr int kId = 9;
+
+// PID
+constexpr double kMaxIntegral = 500000.;
+constexpr double kIZone = 0.;
+constexpr double kDeadband = 0;
+
+constexpr double kP = 0.15;
+constexpr double kI = 0.;
+constexpr double kD = 4.;
+constexpr double kF = 0.06;
+
+constexpr muan::phoenix::SPGains kGains{
+    .p = kP,
+    .i = kI,
+    .d = kD,
+    .f = kF,
+    .i_zone = kIZone,
+    .max_integral = kMaxIntegral,
+    .deadband = kDeadband,
+};
+
+constexpr double kRampTime = 0.1;
+
+constexpr double kFF = 0.06;           // Cancel gravity
+constexpr double kFFSecondStage = 0.;  // Add to kFF to account for stage
+constexpr double kFFCube = 0.01;       // Add to kFF to account for cube
+
+struct ElevatorInput {
+  double encoder;
+  bool hall_effect;
+};
+
+struct ElevatorStatus {
+  double height;
+  bool is_calibrated;
+
+  double unprofiled_goal;
+  double profiled_goal;
+};
+
+struct ElevatorOutput {
+  double voltage;
+  double percent;
+  double current;
+};
+
+class Elevator {
  public:
-  ElevatorController();  // Sets up the controller, observer, plant, and profile
-  void Update(bool outputs_enabled);  // Figures out what the elevator should do
-                                      // and what it's doing based on the
-                                      // outside data
-  void SetGoal(double height);  // Setter for unprofiled_goal_ that also caps it
-                                // to kElevatorMin and Max Height
+  static Elevator& GetInstance();
+  void Update(bool outputs_enabled);
+  void SetGoal(double height);
 
   muan::units::Time TimeLeftUntil(muan::units::Length target,
                                   muan::units::Length final_goal);
 
-  bool is_calibrated() const;  // Getter for if it's calibrated
+  inline bool is_calibrated() const {
+    return hall_calibration_.is_calibrated();
+  }
+
+  inline ElevatorStatus status() { return status_; }
 
  private:
+  Elevator();
+
+  void ReadInputs();
+  void UpdateProfiledGoal(bool outputs_enabled);
+  double CalculateFeedForwards(bool has_cube, bool second_stage);
+
+  muan::phoenix::TalonWrapper elevator_;
+  carriage_canifier::CarriageCanifier& carriage_canifier_ =
+      carriage_canifier::CarriageCanifier::GetInstance();
+  muan::control::HallCalibration hall_calibration_{kHallEffectHeight};
+
+  ElevatorInput input_;
+  ElevatorStatus status_;
+  ElevatorOutput output_;
+
   muan::control::MotionProfilePosition unprofiled_goal_;
   muan::control::MotionProfilePosition profiled_goal_;
 
-  muan::control::MotionProfilePosition UpdateProfiledGoal(
-      bool outputs_enabled);       // Utilizes the trapezoidal motion profile
-  double CapU(double elevator_u);  // Voltage capper to +/- 12
-
-  // Hall Calibration magic (less magical)
-  muan::control::HallCalibration hall_calibration_{kHallEffectHeight};
-
-  // Gain scheduling magic (even less magical)
-  void SetWeights(bool second_stage, bool has_cube);
-
-  // Encoder fault stuff
-  bool encoder_fault_detected_ = false;
-  int num_encoder_fault_ticks_ = 0;
-  double old_pos_;
+  std::mutex talon_lock_;
 };
 
 }  // namespace elevator
-}  // namespace score_subsystem
+}  // namespace subsystems
 }  // namespace c2018
 
-#endif  // C2018_SUBSYSTEMS_SCORE_SUBSYSTEM_ELEVATOR_ELEVATOR_H_
+#endif  // C2018_REWRITE_SUBSYSTEMS_SCORE_SUBSYSTEM_ELEVATOR_ELEVATOR_H_
