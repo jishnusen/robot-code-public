41d56a2d35c472539265469349eb96640b61d824
jishnusen <jishnu1@gmail.com>
2018-05-30 20:23:01 -0700

    No more talon pointers

--- muan/phoenix/talon_wrapper.cpp
+++ muan/phoenix/talon_wrapper.cpp
@@ -3,45 +3,112 @@
 namespace muan {
 namespace phoenix {
 
-TalonWrapper::TalonWrapper(int id) : talon_(CreateTalon(id, SPConfig())) {}
+TalonWrapper::TalonWrapper(int id, SPConfig config) : talon_(id) {
+  talon_.Set(ControlMode::PercentOutput, 0.);  // Safety first!
 
-TalonWrapper::TalonWrapper(int id, SPConfig config)
-    : talon_(CreateTalon(id, config)) {}
+  talon_.ChangeMotionControlFramePeriod(config.motion_control_frame_period);
+  talon_.ClearMotionProfileHasUnderrun(100);
+  talon_.ClearMotionProfileTrajectories();
+
+  talon_.ClearStickyFaults(100);
+
+  talon_.ConfigForwardLimitSwitchSource(LimitSwitchSource_FeedbackConnector,
+                                        LimitSwitchNormal_NormallyOpen,
+                                        100);
+  talon_.ConfigReverseLimitSwitchSource(LimitSwitchSource_FeedbackConnector,
+                                        LimitSwitchNormal_NormallyOpen,
+                                        100);
+  talon_.OverrideLimitSwitchesEnable(config.enable_limit_switch);
+
+  talon_.ConfigSetParameter(ParamEnum::eClearPositionOnLimitF, 0, 0, 0,
+                            100);
+  talon_.ConfigSetParameter(ParamEnum::eClearPositionOnLimitR, 0, 0, 0,
+                            100);
+
+  talon_.ConfigNominalOutputForward(0, 100);
+  talon_.ConfigNominalOutputReverse(0, 100);
+  talon_.ConfigNeutralDeadband(config.neutral_deadband, 100);
+
+  talon_.ConfigPeakOutputForward(1., 100);
+  talon_.ConfigPeakOutputReverse(-1., 100);
+
+  talon_.SetNeutralMode(config.neutral_mode);
+
+  talon_.ConfigForwardSoftLimitThreshold(config.forward_soft_limit, 100);
+  talon_.ConfigForwardSoftLimitEnable(config.enable_soft_limit, 100);
+
+  talon_.ConfigReverseSoftLimitThreshold(config.reverse_soft_limit, 100);
+  talon_.ConfigReverseSoftLimitEnable(config.enable_soft_limit, 100);
+
+  talon_.OverrideSoftLimitsEnable(config.enable_soft_limit);
+
+  talon_.SetInverted(config.inverted);
+  talon_.SetSensorPhase(config.sensor_phase);
+
+  talon_.SelectProfileSlot(0, 0);
+
+  talon_.ConfigVelocityMeasurementPeriod(config.velocity_measurement_period,
+                                         100);
+  talon_.ConfigVelocityMeasurementWindow(config.velocity_measurement_window,
+                                         100);
+
+  talon_.ConfigOpenloopRamp(config.open_loop_ramp_rate, 100);
+  talon_.ConfigClosedloopRamp(config.closed_loop_ramp_rate, 100);
+
+  talon_.ConfigVoltageCompSaturation(0., 100);
+  talon_.ConfigVoltageMeasurementFilter(32, 100);
+  talon_.EnableVoltageCompensation(false);
+
+  talon_.SetStatusFramePeriod(StatusFrameEnhanced::Status_1_General,
+                              config.general_status_frame_rate, 100);
+  talon_.SetStatusFramePeriod(StatusFrameEnhanced::Status_2_Feedback0,
+                              config.feedback_status_frame_rate, 100);
+  talon_.SetStatusFramePeriod(StatusFrameEnhanced::Status_3_Quadrature,
+                              config.quadrature_status_frame_rate, 100);
+  talon_.SetStatusFramePeriod(StatusFrameEnhanced::Status_4_AinTempVbat,
+                              config.analog_temp_vbat_status_frame_rate,
+                              100);
+  talon_.SetStatusFramePeriod(StatusFrameEnhanced::Status_8_PulseWidth,
+                              config.pwm_status_frame_rate, 100);
+
+  talon_.SetControlFramePeriod(ControlFrame::Control_3_General,
+                               config.control_frame_period);
+}
 
 void TalonWrapper::SetOpenloop(double setpoint) {
-  talon_->Set(ControlMode::PercentOutput, setpoint);
+  talon_.Set(ControlMode::PercentOutput, setpoint);
 }
 
 void TalonWrapper::SetPosition(double setpoint, double setpoint_ff) {
-  talon_->Set(ControlMode::Position, setpoint, DemandType_ArbitraryFeedForward,
-              setpoint_ff);
+  talon_.Set(ControlMode::Position, setpoint, DemandType_ArbitraryFeedForward,
+             setpoint_ff);
 }
 
 void TalonWrapper::SetVelocity(double setpoint, double setpoint_ff) {
-  talon_->Set(ControlMode::Velocity, setpoint, DemandType_ArbitraryFeedForward,
-              setpoint_ff);
+  talon_.Set(ControlMode::Velocity, setpoint, DemandType_ArbitraryFeedForward,
+             setpoint_ff);
 }
 
-void TalonWrapper::SetGains(SPGains gains, int slot, int timeout) {
-  talon_->Config_kP(slot, gains.p, 100);
-  talon_->Config_kI(slot, gains.i, 100);
-  talon_->Config_kD(slot, gains.d, 100);
-  talon_->Config_kF(slot, gains.f, 100);
+void TalonWrapper::SetGains(SPGains gains, int slot) {
+  talon_.Config_kP(slot, gains.p, 100);
+  talon_.Config_kI(slot, gains.i, 100);
+  talon_.Config_kD(slot, gains.d, 100);
+  talon_.Config_kF(slot, gains.f, 100);
 
   if (::std::abs(gains.i) > 0.) {
-    talon_->ConfigMaxIntegralAccumulator(slot, gains.max_integral, 100);
-    talon_->Config_IntegralZone(slot, gains.i_zone, 100);
+    talon_.ConfigMaxIntegralAccumulator(slot, gains.max_integral, 100);
+    talon_.Config_IntegralZone(slot, gains.i_zone, 100);
   }
 
-  talon_->ConfigAllowableClosedloopError(slot, gains.deadband, 100);
+  talon_.ConfigAllowableClosedloopError(slot, gains.deadband, 100);
 
   gains_ = gains;
 }
 
-void TalonWrapper::SelectGains(int slot) { talon_->SelectProfileSlot(slot, 0); }
+void TalonWrapper::SelectGains(int slot) { talon_.SelectProfileSlot(slot, 0); }
 
 void TalonWrapper::SetFeedbackSensor(FeedbackDevice sensor) {
-  talon_->ConfigSelectedFeedbackSensor(sensor, 0, 100);
+  talon_.ConfigSelectedFeedbackSensor(sensor, 0, 100);
 }
 
 }  // namespace phoenix
--- muan/phoenix/talon_wrapper.h
+++ muan/phoenix/talon_wrapper.h
@@ -1,7 +1,7 @@
 #ifndef MUAN_PHOENIX_TALON_WRAPPER_H_
 #define MUAN_PHOENIX_TALON_WRAPPER_H_
 
-#include "muan/phoenix/sp_factory.h"
+#include "ctre/Phoenix.h"
 
 namespace muan {
 namespace phoenix {
@@ -16,10 +16,43 @@ struct SPGains {
   double deadband;
 };
 
+struct SPConfig {
+  // Neutral
+  NeutralMode neutral_mode = NeutralMode::Coast;
+  double neutral_deadband = 0.04;
+
+  // Limits
+  bool enable_current_limit = false;
+  bool enable_soft_limit = false;
+  bool enable_limit_switch = false;
+  int forward_soft_limit = 0;
+  int reverse_soft_limit = 0;
+
+  // Phases
+  bool inverted = false;
+  bool sensor_phase = false;
+
+  // Frame periods (ms)
+  int control_frame_period = 5;
+  int motion_control_frame_period = 100;
+  int general_status_frame_rate = 5;
+  int feedback_status_frame_rate = 5;
+  int quadrature_status_frame_rate = 5;
+  int analog_temp_vbat_status_frame_rate = 5;
+  int pwm_status_frame_rate = 5;
+
+  // Velocity Measurement
+  VelocityMeasPeriod velocity_measurement_period =
+      VelocityMeasPeriod::Period_100Ms;
+  int velocity_measurement_window = 64;
+
+  // Ramp rates
+  double open_loop_ramp_rate = 0.;
+  double closed_loop_ramp_rate = 0.;
+};
+
 class TalonWrapper {
  public:
-  TalonWrapper() = default;
-  explicit TalonWrapper(int id);          // Default config (see sp_factory.h)
   TalonWrapper(int id, SPConfig config);  // Specified config
 
   // Set talon output
@@ -28,25 +61,25 @@ class TalonWrapper {
   void SetVelocity(double setpoint, double setpoint_ff);
 
   // PID gains for a given slot
-  void SetGains(SPGains gains, int slot, int timeout = 100);
+  void SetGains(SPGains gains, int slot);
   void SelectGains(int slot);
 
   void SetFeedbackSensor(FeedbackDevice sensor);
   void ResetSensor(double value = 0) {
-    talon_->SetSelectedSensorPosition(value, 0, 0);
+    talon_.SetSelectedSensorPosition(value, 0, 0);
   }
 
   // Getters
-  inline TalonSRX* talon() { return talon_; }
+  inline TalonSRX& talon() { return talon_; }
   inline SPGains gains() { return gains_; }
-  inline double position() { return talon_->GetSelectedSensorPosition(0); }
-  inline double velocity() { return talon_->GetSelectedSensorVelocity(0); }
-  inline double voltage() { return talon_->GetMotorOutputVoltage(); }
-  inline double percent() { return talon_->GetMotorOutputPercent(); }
-  inline double current() { return talon_->GetOutputCurrent(); }
+  inline double position() { return talon_.GetSelectedSensorPosition(0); }
+  inline double velocity() { return talon_.GetSelectedSensorVelocity(0); }
+  inline double voltage() { return talon_.GetMotorOutputVoltage(); }
+  inline double percent() { return talon_.GetMotorOutputPercent(); }
+  inline double current() { return talon_.GetOutputCurrent(); }
 
  private:
-  TalonSRX* talon_;
+  TalonSRX talon_;
   SPGains gains_;
 };
 
