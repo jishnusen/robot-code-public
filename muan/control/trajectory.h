67ffcce2976aef4bafaf255c2a0ad7dda00cb28f
jishnusen <jishnu1@gmail.com>
2018-06-06 15:53:14 -0700

    Get rid of about 50000 useless classes

--- muan/control/trajectory.h
+++ muan/control/trajectory.h
@@ -3,120 +3,34 @@
 
 #include <algorithm>
 #include <vector>
+#include "muan/control/pose.h"
 
 namespace muan {
 namespace control {
 
 template <typename T>
-struct TrajectoryPoint {
-  T state;
-  int index;
-};
-
-template <typename T>
-struct TrajectorySamplePoint {
-  T state;
-  int index_floor;
-  int index_ceil;
-  explicit TrajectorySamplePoint(TrajectoryPoint<T> sample_point);
-  TrajectorySamplePoint(T sample_state, int sample_floor, int sample_ceil);
-};
-
-template <typename T>
 class Trajectory {
  public:
-  explicit Trajectory(std::vector<T> states);
+  explicit Trajectory(std::vector<T> poses);
+  explicit Trajectory(std::vector<Pose> poses);
 
-  TrajectoryPoint<T> GetPoint(int index) const;
-  T GetState(int index) const;
+  T GetPose(int index) const;
 
-  TrajectorySamplePoint<T> Interpolate(double index);
+  T SampleTime(double t) const;
+  T SampleDistance(double s) const;
 
-  bool empty() const { return points_.empty(); }
-  int length() const { return static_cast<int>(points_.size()); }
-  T first_state() const;
-  T last_state() const;
+  std::vector<double> CalculateDistances() const;
+
+  inline bool empty() const { return poses_.empty(); }
+  inline int length() const { return static_cast<int>(poses_.size()); }
+  inline T first_pose() const { return poses_.front(); }
+  inline T last_pose() const { return poses_.back(); }
+  inline double total_distance() const { return CalculateDistances().back(); };
+  inline double start_t() const { return poses_.front().t(); }
+  inline double end_t() const { return poses_.back().t(); }
 
  protected:
-  std::vector<TrajectoryPoint<T>> points_;
-};
-
-template <typename T>
-class IndexView {
- public:
-  explicit IndexView(Trajectory<T> trajectory);
-
-  TrajectorySamplePoint<T> Sample(double interpolant) {
-    return trajectory_.Interpolate(interpolant);
-  }
-
-  double first_interpolant() const { return 0.; }
-  double last_interpolant() const {
-    return ::std::max(0., static_cast<double>(trajectory_.length() - 1));
-  }
-  Trajectory<T> trajectory() const { return trajectory_; }
-
- private:
-  Trajectory<T> trajectory_;
-};
-
-template <typename T>
-class DistanceView {
- public:
-  explicit DistanceView(Trajectory<T> trajectory);
-
-  TrajectorySamplePoint<T> Sample(double interpolant);
-
-  double first_interpolant() const { return 0.; }
-  double last_interpolant() const {
-    return distances_.at(distances_.size() - 1.);
-  }
-  Trajectory<T> trajectory() const { return trajectory_; }
-
- private:
-  Trajectory<T> trajectory_;
-  std::vector<double> distances_;
-};
-
-template <typename T>
-class TimedView {
- public:
-  explicit TimedView(Trajectory<T> trajectory);
-
-  TrajectorySamplePoint<T> Sample(double interpolant);
-
-  double first_interpolant() const { return start_t_; }
-  double last_interpolant() const { return end_t_; }
-  Trajectory<T> trajectory() const { return trajectory_; }
-
- private:
-  Trajectory<T> trajectory_;
-  double start_t_, end_t_;
-};
-
-template <typename T, typename S>
-class TrajectoryIterator {
- public:
-  explicit TrajectoryIterator(S view);
-
-  bool done() const { return remaining_progress() < 1e-10; }
-  double progress() const { return progress_; }
-  double remaining_progress() const {
-    return view_.last_interpolant() - progress_;
-  }
-
-  TrajectorySamplePoint<T> current_sample() const { return current_sample_; }
-  T current_state() const { return current_sample().state(); }
-
-  Trajectory<T> trajectory() const { return view_.trajectory(); }
-
-  TrajectorySamplePoint<T> Advance(double additional_progress);
-  TrajectorySamplePoint<T> Preview(double additional_progress);
-
- private:
-  double progress_;
-  TrajectorySamplePoint<T> current_sample_;
-  S view_;
+  std::vector<T> poses_;
 };
 
 }  // namespace control
--- muan/control/trajectory.hpp
+++ muan/control/trajectory.hpp
@@ -10,162 +10,71 @@ namespace muan {
 namespace control {
 
 template <typename T>
-TrajectorySamplePoint<T>::TrajectorySamplePoint(TrajectoryPoint<T> sample_point)
-    : state(sample_point.state),
-      index_floor(sample_point.index),
-      index_ceil(sample_point.index) {}
+Trajectory<T>::Trajectory(std::vector<T> poses) : poses_(poses) {}
 
 template <typename T>
-TrajectorySamplePoint<T>::TrajectorySamplePoint(T sample_state,
-                                                int sample_floor,
-                                                int sample_ceil)
-    : state(sample_state), index_floor(sample_floor), index_ceil(sample_ceil) {}
+T Trajectory<T>::GetPose(int index) const {
+  return poses_.at(index);
+}
 
 template <typename T>
-Trajectory<T>::Trajectory(std::vector<T> states) {
-  points_ = std::vector<TrajectoryPoint<T>>(states.size());
-  int i = 0;
-  for (T state : states) {
-    points_.at(i) = {state, i};
-    i++;
+T Trajectory<T>::SampleTime(double t) const {
+  if (t <= start_t()) {
+    return first_pose();
+  } else if (t >= end_t()) {
+    return last_pose();
   }
-}
 
-template <typename T>
-TrajectoryPoint<T> Trajectory<T>::GetPoint(int index) const {
-  return points_.at(index);
-}
-
-template <typename T>
-T Trajectory<T>::GetState(int index) const {
-  return GetPoint(index).state;
-}
-
-template <typename T>
-T Trajectory<T>::first_state() const {
-  return GetState(0);
-}
-
-template <typename T>
-T Trajectory<T>::last_state() const {
-  return GetState(points_.size() - 1);
-}
-
-template <typename T>
-TrajectorySamplePoint<T> Trajectory<T>::Interpolate(double index) {
-  if (index <= 0.0) {
-    return TrajectorySamplePoint<T>(GetPoint(0));
-  } else if (index >= length() - 1) {
-    return TrajectorySamplePoint<T>(GetPoint(length() - 1));
-  }
-  int i = static_cast<int>(floor(index));
-  double frac = index - i;
-  if (frac < 1e-10) {
-    return TrajectorySamplePoint<T>(GetPoint(i));
-  } else if (frac > 1.0 - 1e-10) {
-    return TrajectorySamplePoint<T>(GetPoint(i + 1));
-  } else {
-    return TrajectorySamplePoint<T>(
-        GetState(i).Interpolate(GetState(i + 1), frac), i, i + 1);
-  }
-}
-
-template <typename T>
-IndexView<T>::IndexView(Trajectory<T> trajectory) : trajectory_(trajectory) {}
-
-template <typename T>
-DistanceView<T>::DistanceView(Trajectory<T> trajectory)
-    : trajectory_(trajectory) {
-  distances_ = std::vector<double>(trajectory.length());
-  for (int i = 1; i < trajectory.length(); i++) {
-    distances_.at(i) = distances_.at(i - 1) +
-                       (trajectory_.GetState(i) - trajectory_.GetState(i - 1))
-                           .translational()
-                           .norm();
-  }
-}
-
-template <typename T>
-TrajectorySamplePoint<T> DistanceView<T>::Sample(double interpolant) {
-  if (interpolant >= last_interpolant()) {
-    return TrajectorySamplePoint<T>(
-        trajectory_.GetPoint(trajectory_.length() - 1));
-  } else if (interpolant <= 0.) {
-    return TrajectorySamplePoint<T>(trajectory_.GetPoint(0));
-  }
-  for (int i = 1; i < static_cast<int>(distances_.size()); i++) {
-    TrajectoryPoint<T> sample = trajectory_.GetPoint(i);
-    if (distances_.at(i) > interpolant) {
-      if (::std::abs(distances_.at(i) - distances_.at(i - 1)) < 1e-9) {
-        return TrajectorySamplePoint<T>(sample);
+  for (int i = 1; i < length(); i++) {
+    T sample = GetPose(i);
+    if (sample.t() >= t) {
+      T prev_sample = GetPose(i - 1);
+      if (::std::abs(sample.t() - prev_sample.t()) < 1e-9) {
+        return sample;
       } else {
-        TrajectoryPoint<T> prev_sample = trajectory_.GetPoint(i - 1);
-        return TrajectorySamplePoint<T>(
-            prev_sample.state.Interpolate(
-                sample.state, (interpolant - distances_.at(i - 1)) /
-                                  (distances_.at(i) - distances_.at(i - 1))),
-            i - 1, i);
+        return prev_sample.Interpolate(
+            sample, (t - prev_sample.t()) / (sample.t() - prev_sample.t()));
       }
     }
   }
-  return TrajectorySamplePoint<T>(trajectory_.GetPoint(0));
+  return poses_.at(0);
 }
 
 template <typename T>
-TimedView<T>::TimedView(Trajectory<T> trajectory)
-    : trajectory_(trajectory),
-      start_t_(trajectory.first_state().t()),
-      end_t_(trajectory.last_state().t()) {}
+std::vector<double> Trajectory<T>::CalculateDistances() const {
+  std::vector<double> distances(length());
+  distances.at(0) = 0.;
+  for (int i = 1; i < length(); i++) {
+    distances.at(i) = distances.at(i - 1) +
+                      (GetPose(i) - GetPose(i - 1)).translational().norm();
+  }
+  return distances;
+}
 
 template <typename T>
-TrajectorySamplePoint<T> TimedView<T>::Sample(double interpolant) {
-  if (interpolant >= end_t_) {
-    return TrajectorySamplePoint<T>(
-        trajectory_.GetPoint(trajectory_.length() - 1));
-  } else if (interpolant <= start_t_) {
-    return TrajectorySamplePoint<T>(trajectory_.GetPoint(0));
+T Trajectory<T>::SampleDistance(double s) const {
+  std::vector<double> distances = CalculateDistances();
+  if (s <= 0) {
+    return first_pose();
+  } else if (s >= distances.back()) {
+    return last_pose();
   }
-  for (int i = 1; i < trajectory_.length(); i++) {
-    TrajectoryPoint<T> sample = trajectory_.GetPoint(i);
-    if (sample.state.t() >= interpolant) {
-      TrajectoryPoint<T> prev_sample = trajectory_.GetPoint(i);
-      if (::std::abs(sample.state.t() - prev_sample.state.t()) < 1e-9) {
-        return TrajectorySamplePoint<T>(sample);
+
+  for (int i = 1; i < static_cast<int>(distances.size()); i++) {
+    T sample = GetPose(i);
+    if (distances.at(i) > s) {
+      if (::std::abs(distances.at(i) - distances.at(i - 1)) < 1e-9) {
+        return sample;
       } else {
-        return TrajectorySamplePoint<T>(
-            prev_sample.state.Interpolate(
-                sample.state, (interpolant - prev_sample.state.t()) /
-                                  (sample.state.t() - prev_sample.state.t())),
-            i - 1, i);
+        T prev_sample = GetPose(i - 1);
+        return prev_sample.Interpolate(
+            sample, (s - distances.at(i - 1)) /
+                        (distances.at(i) - distances.at(i - 1)));
       }
     }
   }
-  return TrajectorySamplePoint<T>(trajectory_.GetPoint(0));
-}
 
-template <typename T, typename S>
-TrajectoryIterator<T, S>::TrajectoryIterator(S view)
-    : progress_(view.first_interpolant()),
-      current_sample_(view.Sample(view.first_interpolant())),
-      view_(view) {}
-
-template <typename T, typename S>
-TrajectorySamplePoint<T> TrajectoryIterator<T, S>::Advance(
-    double additional_progress) {
-  progress_ = ::std::max(
-      view_.first_interpolant(),
-      ::std::min(view_.last_interpolant(), progress_ + additional_progress));
-  current_sample_ = view_.Sample(progress_);
-  return current_sample_;
-}
-
-template <typename T, typename S>
-TrajectorySamplePoint<T> TrajectoryIterator<T, S>::Preview(
-    double additional_progress) {
-  double progress = ::std::max(
-      view_.first_interpolant(),
-      ::std::min(view_.last_interpolant(), progress_ + additional_progress));
-  return view_.Sample(progress);
+  return poses_.at(0);
 }
 
 }  // namespace control
