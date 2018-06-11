19b8d14aec5cd9df02f4632a76d9e35bb1e52f88
jishnusen <jishnu1@gmail.com>
2018-06-11 12:54:00 -0700

    Add mock tests for talon and victor wrappers

---
+++ muan/phoenix/mock/victor_wrapper.cpp
@@ -0,0 +1,38 @@
+#include "muan/phoenix/mock/victor_wrapper.h"
+#include "muan/units/units.h"
+
+namespace muan {
+namespace phoenix {
+
+VictorWrapper::VictorWrapper(int id, Config config)
+    : id_(id), conversion_factor_(config.conversion_factor) {}
+
+void VictorWrapper::SetOpenloopGoal(double setpoint) {  // Voltage
+  open_loop_voltage_ = setpoint;
+}
+
+void VictorWrapper::SetPositionGoal(double setpoint,
+                                   double setpoint_ff) {  // Position, Voltage
+  (void)setpoint_ff;
+  prev_position_ = position_;
+  position_ = setpoint * conversion_factor_;
+}
+
+void VictorWrapper::SetVelocityGoal(double setpoint,
+                                   double setpoint_ff) {  // Velocity, Voltage
+  (void)setpoint_ff;
+  prev_position_ = position_;
+  position_ += setpoint * 10 * ms;
+}
+
+void VictorWrapper::SetGains(Gains gains, int slot) {
+  (void)gains;
+  (void)slot;
+}
+
+void VictorWrapper::SelectGains(int slot) {
+  (void)slot;
+}
+
+}  // namespace phoenix
+}  // namespace muan
