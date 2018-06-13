3f653249c2ce93281f129042cd93874dae696774
jishnusen <jishnu1@gmail.com>
2018-06-12 17:30:21 -0700

    Fix up the pose test to not use implicit typecasting

--- muan/control/pose_test.cpp
+++ muan/control/pose_test.cpp
@@ -5,7 +5,7 @@ namespace muan {
 namespace control {
 
 TEST(Pose, HeadingWrapping) {
-  Pose a = (Eigen::Vector3d() << 1.0, 2.0, 0.0).finished();
+  Pose a((Eigen::Vector3d() << 1.0, 2.0, 0.0).finished());
   Pose b = a.RotateBy(M_PI);
   EXPECT_NEAR(b.translational()(0), -1.0, 1e-6);
   EXPECT_NEAR(b.translational()(1), -2.0, 1e-6);
@@ -13,13 +13,13 @@ TEST(Pose, HeadingWrapping) {
   // Either the heading should be -pi or pi, it doesn't matter
   EXPECT_NEAR(::std::abs(b.heading()), M_PI, 1e-6);
 
-  Pose c = (Eigen::Vector3d() << 0.0, 0.0, -1.0).finished();
+  Pose c((Eigen::Vector3d() << 0.0, 0.0, -1.0).finished());
   EXPECT_NEAR((b + c).heading(), M_PI - 1.0, 1e-6);
 }
 
 TEST(Pose, Compose) {
-  Pose a = (Eigen::Vector3d() << 2.0, 0.0, M_PI / 4.0).finished();
-  Pose b = (Eigen::Vector3d() << 1.0, 2.0, M_PI).finished();
+  Pose a((Eigen::Vector3d() << 2.0, 0.0, M_PI / 4.0).finished());
+  Pose b((Eigen::Vector3d() << 1.0, 2.0, M_PI).finished());
   Pose c = a.Compose(b);
 
   EXPECT_NEAR(c.translational()(0), 2.0 - ::std::sqrt(2.0) / 2.0, 1e-6);
