d763d9f52e79832cd5240b6ac2b12b269bcb527d
jishnusen <jishnu1@gmail.com>
2018-06-14 19:29:51 -0700

    Add nonlinear feedback controller

---
+++ muan/control/nonlinear_feedback_controller.h
@@ -0,0 +1,31 @@
+#ifndef MUAN_CONTROL_NONLINEAR_FEEDBACK_CONTROLLER_H_
+#define MUAN_CONTROL_NONLINEAR_FEEDBACK_CONTROLLER_H_
+
+#include <Eigen/Core>
+#include "muan/control/drivetrain_model.h"
+#include "muan/control/pose.h"
+
+namespace muan {
+namespace control {
+
+class NonLinearFeedbackController {
+ public:
+  struct Output {
+    Eigen::Vector2d velocity;      // left, right
+    Eigen::Vector2d feedforwards;  // left, right
+  };
+
+  NonLinearFeedbackController(DrivetrainModel model, double beta, double zeta);
+  Output Update(Eigen::Vector2d velocity, Pose current, Pose error,
+                bool high_gear) const;
+
+ private:
+  DrivetrainModel model_;
+  double beta_;
+  double zeta_;
+};
+
+}  // namespace control
+}  // namespace muan
+
+#endif  //  MUAN_CONTROL_NONLINEAR_FEEDBACK_CONTROLLER_H_
