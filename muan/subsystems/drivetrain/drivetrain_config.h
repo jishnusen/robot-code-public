9b6e48d3c909332be2ca9b7bc2a67abf4905c13c
jishnusen <jishnu1@gmail.com>
2018-08-06 15:30:56 -0700

    Add a generic drivetrain subsystem
    This subsystem will be instatiated in all future robot codes. To adapt
    to different drivetrains, a DrivetrainConfig struct will be customized.
    
    This subsystem includes both an open-loop (blatant rip of CheesyDrive)
    and a closed-loop (spline-following via a Nonlinear Feedback
    controller).
    
    There are no unit tests for a reason; unit testing a drivetrain without
    (or even with) a model is too big a hassle to be worth it. Physical
    tests will suffice.

---
+++ muan/subsystems/drivetrain/drivetrain_config.h
@@ -0,0 +1,28 @@
+#ifndef MUAN_SUBSYSTEMS_DRIVETRAIN_DRIVETRAIN_CONFIG_H_
+#define MUAN_SUBSYSTEMS_DRIVETRAIN_DRIVETRAIN_CONFIG_H_
+
+#include "muan/control/drivetrain_model.h"
+
+namespace muan {
+namespace subsystems {
+namespace drivetrain {
+
+struct DrivetrainConfig {
+  double wheel_non_linearity = 1.;
+  double sensitivity = 1.;
+
+  double beta = 1.;
+  double zeta = 1.;
+
+  double dt = 0.01;
+
+  muan::control::DriveTransmission::Properties high_gear_properties;
+  muan::control::DriveTransmission::Properties low_gear_properties;
+  muan::control::DrivetrainModel::Properties drive_properties;
+};
+
+}  // namespace drivetrain
+}  // namespace subsystems
+}  // namespace muan
+
+#endif  // MUAN_SUBSYSTEMS_DRIVETRAIN_DRIVETRAIN_CONFIG_H_
