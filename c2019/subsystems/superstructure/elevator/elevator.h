5cdf356d93c5253aae42422daa819fb4eb32c6d2
jishnusen <jishnu1@gmail.com>
2019-02-16 14:11:03 -0800

    Fix max

--- c2019/subsystems/superstructure/elevator/elevator.h
+++ c2019/subsystems/superstructure/elevator/elevator.h
@@ -11,7 +11,7 @@ namespace elevator {
 
 // Height constraints
 constexpr double kMinHeight = 0;
-constexpr double kMaxHeight = 1.74;
+constexpr double kMaxHeight = 1.79;
 constexpr double kSecondStageHeight = 1.;
 
 // Setting max voltage so that people don't die
